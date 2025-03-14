7eb353c385609b5086024c8cd577a9827da8287f
Jacob Calvillo <jakeob99@gmail.com>
2025-03-13 19:55:07 -0600

    adding supabase

--- build/routes/email.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.router = void 0;
-const email_controller_1 = require("../controllers/email.controller");
-const express_1 = require("express");
-const router = (0, express_1.Router)();
-exports.router = router;
-router.post('/email/welcome', email_controller_1.sendWelcomeEmailController);
--- build/routes/webhook.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.router = void 0;
-const express_1 = require("express");
-const webhook_controller_1 = require("../webhook/webhook.controller");
-const router = (0, express_1.Router)();
-exports.router = router;
-router.post('/webhook', webhook_controller_1.stripeWebhookController);
